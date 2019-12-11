806d7f7533be8e154fb9173019ab6aa7102b0967
luisdev97 <amadormontorodavid@gmail.com>
2019-12-11 23:03:19 +0100

    Init proyect

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="app container">
+      
+      <div className="jumbotron">
+        <p className="lead text-center">React Images Search</p>
+      </div>
+
+      <div className="row justify-content-center">
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
