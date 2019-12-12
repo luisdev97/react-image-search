f5dd4f6009afb321d02996bd4009b2e0ac954728
luisdev97 <amadormontorodavid@gmail.com>
2019-12-12 20:21:53 +0100

    Images pagination

--- src/App.js
+++ src/App.js
@@ -7,14 +7,27 @@ function App() {
 
   const [ keyword, setKeyWord ] = useState('');
   const [ images, setImages ] = useState([]); 
+  const [ actualPage, setActualPage ] = useState(1);
+  const [ numPages, setNumPages ] = useState(0)
 
+ 
   const getImagesFromAPI = async() => {
+
     if(keyword !== ''){
+      
       const imagesPerPage = 30;
       const key = "14604796-072f733d726e080372ea0015c"
-      const url = `https://pixabay.com/api/?key=${key}&q=${keyword}&per_page=${imagesPerPage }`;
+      const url = `https://pixabay.com/api/?key=${key}&q=${keyword}&per_page=${imagesPerPage}&page=${actualPage}`;
       const res = await axios.get(url);
+
       setImages(res.data.hits);
+
+      const totalPages = Math.ceil(res.data.totalHits / imagesPerPage);
+      setNumPages(totalPages);
+
+      const jumbotron = document.querySelector('.jumbotron');
+      jumbotron.scrollIntoView({ behavior: 'smooth', block: 'end' });
+
     }
    
     
@@ -22,7 +35,7 @@ function App() {
 
   useEffect(() => {
     getImagesFromAPI();
-  },[keyword]);
+  },[keyword, actualPage]);
 
   return (
     <div className="app container">
@@ -34,6 +47,16 @@ function App() {
 
       <div className="row justify-content-center">
         <Imageslist images={ images }/>
+
+        { actualPage === 1 ? null : 
+          <button onClick={ () => { setActualPage(actualPage - 1 )} } className="btn btn-info mr-1">Prev &laquo;</button>
+        }
+
+        { actualPage === numPages ? null :
+          <button onClick={ () => { setActualPage(actualPage + 1 )} } className="btn btn-info mr-1">Next &raquo;</button>
+        }
+        
+        
       </div>
     </div>
   );
